35eb945fc05f6e45c2e5baf81681f6dbd13dc7c9
yshmn-ph <ronnakorn.pho@student.mahidol.edu>
2025-10-02 04:10:37 +0000

    New main

--- main.cpp
+++ main.cpp
@@ -1,38 +1,49 @@
 #include <iostream>
 #include <iomanip>
-#define SIZE 10
+
 
 using namespace std;
 
 int main(int argc,char *argv[]){
   int *pa, *pb, i, temp;
-int a[SIZE] ={1,2,3,4,5,6,7,8,9,10}; 
-pa = &a[0]; //pa =a;
+  int n;
+  cout << "How many numbers?";
+  cin >> n;
+  pa = new int [n];
+
+  for ( i = 0; i < n; i ++, pa++){
+    cout<<"Input "<<":";
+    cin>>*pa;
+  }
+  
+  pa = pa - n ;
+  
 cout<<"Original: ";
-for (i=0; i<SIZE-1; i++ ,pa++){
+for (i=0; i<n-1; i++ ,pa++){
    cout<<setw(3)<<*pa ;//<<endl;
   //cout<<pa<<endl;
 }
 cout<<setw(3)<<*pa<<endl;
 
-pa = &a[0];  
-pb = &a[SIZE-1];
-for (i=0; i<SIZE/2; i++)
+pb = pa ;
+pa-= (n-1);
+
+for (i=0; i<n/2; i++)
 {
    temp = *pa;  
    *pa = *pb;  
    *pb = temp;
    pa++;  pb--;
 }
-pa=&a[0];
-pb-=SIZE/2;
+pa -= (n/2);
+//pb-=n/2;
 cout<<"Reversed: ";
-for (i=0; i<SIZE-1; i++ ,pa++){
+for (i=0; i<n-1; i++ ,pa++){
    cout<<setw(3)<<*pa ;//<<endl;
   //cout<<pa<<endl;
 }
 cout<<setw(3)<<*pa<<endl;
 
-
+delete []pa;
 return 0;
 }
